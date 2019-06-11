0a915242fb9da0c8159236e4c0ef6b72254856a6
David Lee <devdavidlee@gmail.com>
2019-06-11 16:51:26 -0700

    try to get dev tools to work in prod

--- src/utils/governorMiddleware.ts
+++ src/utils/governorMiddleware.ts
@@ -3,45 +3,54 @@ import IState from '../interfaces/IState';
 const instanceID = new Date().getTime();
 
 export default (initialState: IState, dispatch: any) => {
-  const reduxDevTools = (window as any).__REDUX_DEVTOOLS_EXTENSION__;
+  let devTools: any = null;
 
-  if (reduxDevTools) {
-    const name = `react-governor - ${instanceID}`;
-    const features = {
-      jump: true,
-    };
+  const initDevTools = () => {
+    const reduxDevTools = (window as any).__REDUX_DEVTOOLS_EXTENSION__;
+    if (reduxDevTools) {
+      const name = `react-governor - ${instanceID}`;
+      const features = { jump: true };
 
-    const devTools = reduxDevTools.connect({ name, features });
-
-    devTools.subscribe((data: any) => {
-      switch (data.type) {
-        case 'START':
-          devTools.init(initialState);
-          break;
-        case 'RESET':
-          dispatch({ newState: initialState });
-          break;
-        case 'DISPATCH':
-          switch (data.payload.type) {
-            case 'JUMP_TO_STATE':
-            case 'JUMP_TO_ACTION': {
-              dispatch({
-                newState: JSON.parse(data.state),
-              });
+      devTools = reduxDevTools.connect({ name, features });
+      if (devTools) {
+        devTools.subscribe((data: any) => {
+          switch (data.type) {
+            case 'START':
+              devTools.init(initialState);
               break;
-            }
+            case 'RESET':
+              dispatch({ newState: initialState });
+              break;
+            case 'DISPATCH':
+              switch (data.payload.type) {
+                case 'JUMP_TO_STATE':
+                case 'JUMP_TO_ACTION': {
+                  dispatch({
+                    newState: JSON.parse(data.state),
+                  });
+                  break;
+                }
+                default:
+                  break;
+              }
+              break;
             default:
               break;
           }
-          break;
-        default:
-          break;
+        });
       }
-    });
+    }
+  };
 
-    return (actionKey: string, state: IState) => {
-      devTools.send({ type: actionKey }, state, {}, instanceID);
-      return state;
-    };
-  }
+  initDevTools();
+
+  return (actionKey: string, state: IState) => {
+    // to get dev tools working in production
+    if (!devTools) {
+      initDevTools();
+    }
+
+    devTools.send({ type: actionKey }, state, {}, instanceID);
+    return state;
+  };
 };
