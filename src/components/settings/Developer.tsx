74c1bf1eddef994f1cc0997dd1fb57a148e99d8c
David Lee <devdavidlee@gmail.com>
2019-06-10 22:24:09 -0700

    add commands to developer tab

--- src/components/settings/Developer.tsx
+++ src/components/settings/Developer.tsx
@@ -2,14 +2,61 @@ import React, { FC } from 'react';
 import { useContext } from '../../context';
 import ReactJson from 'react-json-view';
 
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
+
 import BasePage from './BasePage';
 import BaseSection from './BaseSection';
 
 const Developer: FC = () => {
+  const classes = useStyles();
   const [context] = useContext();
 
   return (
     <BasePage title="Developer">
+      <BaseSection title="Commands">
+        {(context.app.operatingSystem === 'win32' ||
+          context.app.operatingSystem === 'darwin') && (
+          <>
+            <Typography className={classes.command} variant="body2">
+              Open dev tools:
+              <span className={classes.keys}>
+                <span>Ctrl</span> + <span>Alt</span> + <span>I</span>
+              </span>
+              or
+              <span className={classes.keys}>
+                <span>F12</span>
+              </span>
+            </Typography>
+            <Typography className={classes.command} variant="body2">
+              Refresh application:
+              <span className={classes.keys}>
+                <span>Ctrl</span> + <span>R</span>
+              </span>
+            </Typography>
+          </>
+        )}
+        {context.app.operatingSystem === 'mac' && (
+          <>
+            <Typography className={classes.command} variant="body2">
+              Open dev tools:
+              <span className={classes.keys}>
+                <span>Cmd</span> + <span>Alt</span> + <span>I</span>
+              </span>
+              or
+              <span className={classes.keys}>
+                <span>F12</span>
+              </span>
+            </Typography>
+            <Typography className={classes.command} variant="body2">
+              Refresh application:
+              <span className={classes.keys}>
+                <span>Cmd</span> + <span>R</span>
+              </span>
+            </Typography>
+          </>
+        )}
+      </BaseSection>
       <BaseSection title="Global State">
         <ReactJson
           src={context}
@@ -23,5 +70,21 @@ const Developer: FC = () => {
     </BasePage>
   );
 };
+
+const useStyles: any = makeStyles((theme: any) => ({
+  command: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  keys: {
+    padding: theme.spacing(0, 1),
+    fontSize: '0.8rem',
+
+    '& span': {
+      backgroundColor: theme.palette.backgroundAccent,
+      padding: theme.spacing(0.5, 1),
+    },
+  },
+}));
 
 export default Developer;
