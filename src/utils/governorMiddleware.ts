6016fe9232d0190f645c636387f3b6711b07c371
David Lee <devdavidlee@gmail.com>
2019-08-21 19:48:45 -0700

    fix governor middleware

--- src/utils/governorMiddleware.ts
+++ src/utils/governorMiddleware.ts
@@ -3,25 +3,29 @@ import IState from '../interfaces/IState';
 const instanceID = new Date().getTime();
 const reduxDevTools = (window as any).__REDUX_DEVTOOLS_EXTENSION__;
 
-export default (initialState: IState, dispatch: any) => {
-  let devTools: any = null;
+let devTools: any = null;
 
+export default (initialState: IState, dispatch: any) => {
   const initDevTools = () => {
     if (reduxDevTools) {
       const name = `react-governor - ${instanceID}`;
       const features = { jump: true };
 
-      devTools = reduxDevTools.connect({ name, features });
-      if (devTools) {
+      if (!devTools) {
+        devTools = reduxDevTools.connect({ name, features });
+      } else {
         devTools.subscribe((data: any) => {
           switch (data.type) {
             case 'START':
+              console.log('1');
               devTools.init(initialState);
               break;
             case 'RESET':
+              console.log('2');
               dispatch({ newState: initialState });
               break;
             case 'DISPATCH':
+              console.log('3');
               switch (data.payload.type) {
                 case 'JUMP_TO_STATE':
                 case 'JUMP_TO_ACTION': {
